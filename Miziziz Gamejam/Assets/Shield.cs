df9ebb9ae37e42552ae5f8049aa6fb662bf8df47
Casterra <mdgreen155@yahoo.com>
2020-08-19 12:30:02 -0400

    Movement and very simple AI

---
+++ Miziziz Gamejam/Assets/Shield.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public Transform target;
+    public float fRadius = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v3Pos = Camera.main.WorldToScreenPoint(target.position);
+        v3Pos = Input.mousePosition - v3Pos;
+        float angle = Mathf.Atan2(v3Pos.y, v3Pos.x) * Mathf.Rad2Deg;
+        v3Pos = Quaternion.AngleAxis(angle, Vector3.forward) * (Vector3.right * fRadius);
+        transform.position = target.position + v3Pos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Arrow"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
