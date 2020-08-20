3aa3e901d04f95a8e2a987e9d3568a61134a8709
Casterra <mdgreen155@yahoo.com>
2020-08-19 21:46:48 -0400

    Avg AI

--- Miziziz Gamejam/Assets/Arrow.cs
+++ Miziziz Gamejam/Assets/Arrow.cs
@@ -6,16 +6,32 @@ public class Arrow : MonoBehaviour
 {
     public float spd = 1.5f;
 
+    private GameObject player;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        var moveDir = (player.transform.position - transform.position).normalized * spd;
+        rb.velocity = new Vector2(moveDir.x, moveDir.y);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(1);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
