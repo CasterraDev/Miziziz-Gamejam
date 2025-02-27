c608232f01bbd36dee305947b5af3689bb927c0c
CasterraDev <casterradev@gmail.com>
2025-02-26 19:42:31 -0500

    Movement and very simple AI

---
+++ Miziziz Gamejam/Assets/ShooterEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterEnemy : MonoBehaviour
+{
+    public GameObject player;
+    public float spd = 2f;
+    public float range = 1.5f;
+    public GameObject bulletPrefab;
+    public float shootingCooldown = 2f;
+
+    private float shooterTimer;
+    private float dist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        shooterTimer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, player.transform.position);
+
+        if (dist > range)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, spd * Time.deltaTime);
+        }
+        else
+        {
+            if (shooterTimer < Time.time)
+            {
+                var bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                var moveDir = (player.transform.position - transform.position).normalized * spd;
+                bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(moveDir.x, moveDir.y);
+                Vector3 rot = (player.transform.position - transform.position);
+                bullet.transform.rotation = ;
+                shooterTimer = Time.time + shootingCooldown;
+            }
+        }
+    }
+}
---
+++ Miziziz Gamejam/Assets/playerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float spd = 2f;
+
+    private float moveLimiter = .7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+
+        if (h != 0 && v != 0)
+        {
+            h *= moveLimiter;
+            v *= moveLimiter;
+        }
+
+        transform.Translate(new Vector3(h * spd * Time.deltaTime, v * spd * Time.deltaTime));
+    }
+}
